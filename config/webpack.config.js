2a1f92426f0b2c7136ab8b763c31d195d41d43ec
David Durst <davidbdurst@gmail.com>
2016-08-18 15:54:37 -0400

    added babel rc, react, babel-polyfill

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,11 +1,20 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
-    entry: './src/app.js',
+    entry: ['babel-polyfill', './src/app.js'],
     output: {
         path: './bin',
         filename: 'app.bundle.js'
     },
     plugins: [new HtmlWebpackPlugin({
         title: "Hello World Title"
-    })]
+    })],
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel'
+            }
+        ]
+    }
 }
